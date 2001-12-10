6940985648f7b86a4f4c36a5eaf345b62fec5571
vondo <>
2001-12-10 14:15:26 +0000

    better javascript standards compliance

--- DocDB/cgi/Scripts.pm
+++ DocDB/cgi/Scripts.pm
@@ -1,14 +1,16 @@
 sub HelpPopupScript {
-  print "<script LANGUAGE=\"JavaScript\">\n";
+  print "<script LANGUAGE=\"JavaScript\" type=\"text/javascript\">\n";
+  print "<!-- \n";
 
   print "function helppopupwindow(page){\n";
   print "window.open(page,\"docdbhelp\",\"width=450,height=350,menubar=0,resizable=1,scrollbars=1,status=0,titlebar=0,toolbar=0,left=0,top=0\");\n";
   print "}\n";
 
+  print "//-->\n";
   print "</script>\n";
 }
 
-sub HelpLink {
+sub HelpLink { #FIXME Don't hard-code link
   my ($helpterm) = @_;
   print " href=\"Javascript:helppopupwindow(\'DocDBHelp?term=$helpterm\');\">";
 }
