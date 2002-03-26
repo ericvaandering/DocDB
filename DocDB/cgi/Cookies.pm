ad375a92a50884108064579c18893623ab6948d5
vondo <>
2002-03-26 01:45:17 +0000

    Corrections and notes to Adam's branch

--- DocDB/cgi/Cookies.pm
+++ DocDB/cgi/Cookies.pm
@@ -1,11 +1,12 @@
 sub GetPrefsCookie {
-  $AuthorIDPref = $query->cookie('authorid');
-  $UploadTypePref = $query->cookie('archive');
-  $NumFilesPref = $query->cookie('numfile');
-  $UploadMethodPref = $query->cookie('upload');
-  $TopicModePref = $query->cookie('topicmode');
-  $AuthorModePref = $query->cookie('authormode');
-  $DateOverridePref = $query->cookie('overdate');
+  $AuthorIDPref     = $query -> cookie('authorid'); # Maybe UserIDPref instead
+  $UploadTypePref   = $query -> cookie('archive');
+  $NumFilesPref     = $query -> cookie('numfile');
+  $UploadMethodPref = $query -> cookie('upload');
+  $TopicModePref    = $query -> cookie('topicmode');
+  $AuthorModePref   = $query -> cookie('authormode');
+  $DateOverridePref = $query -> cookie('overdate');
 }
 
 1;
+ 
