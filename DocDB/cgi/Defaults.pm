fa3889732aff2edb783669a53013afe1de89e3d9
vondo <>
2002-03-27 19:38:17 +0000

    Change in handling of Over-date, check box method not compatible with cookie

--- DocDB/cgi/Defaults.pm
+++ DocDB/cgi/Defaults.pm
@@ -1,3 +1,16 @@
+#  Module Purpose:
+#    Gather in one place all the routines that set the look and feel
+#    of DocumentAddForm based on user selections, preferences, and defaults
+#    (in that order)
+#
+#  Functions in this file:
+#    
+#    SetAuthorMode:   Selectable list or free-form text field
+#    SetTopicMode:    Single or multiple selectable lists 
+#    SetUploadMethod: File upload or HTTP fetch
+#    SetDateOverride: Allows over-riding modification date  
+#  
+
 sub SetAuthorMode {
   if ($params{authormode}) {
     $AuthorMode = $params{authormode};
@@ -28,6 +41,14 @@ sub SetUploadMethod {
   }  
   if ($Upload ne "http" && $Upload ne "file") {
     $Upload = "file";
+  }  
+}
+
+sub SetDateOverride {
+  if ($params{overdate}) {
+    $Overdate = $params{overdate};
+  } else {
+    $Overdate = $DateOverridePref;
   }  
 }
 
