cb890e48e87bd1136406ee3d8103f88ef088cd63
vondo <>
2002-03-27 21:09:48 +0000

    File counts and authors

--- DocDB/cgi/Defaults.pm
+++ DocDB/cgi/Defaults.pm
@@ -5,11 +5,12 @@
 #
 #  Functions in this file:
 #    
-#    SetAuthorMode:   Selectable list or free-form text field
-#    SetTopicMode:    Single or multiple selectable lists 
-#    SetUploadMethod: File upload or HTTP fetch
-#    SetDateOverride: Allows over-riding modification date  
-#  
+#    SetAuthorMode:    Selectable list or free-form text field
+#    SetTopicMode:     Single or multiple selectable lists 
+#    SetUploadMethod:  File upload or HTTP fetch
+#    SetDateOverride:  Allows over-riding modification date  
+#    SetAuthorDefault: Sets Author and Requester defaults to cookie value
+#    SetFileOptions:   Sets archive mode and number of uploads
 
 sub SetAuthorMode {
   if ($params{authormode}) {
@@ -51,5 +52,55 @@ sub SetDateOverride {
     $Overdate = $DateOverridePref;
   }  
 }
+
+sub SetAuthorDefault {
+  if ($AuthorIDPref) {
+    @AuthorDefaults = ($AuthorIDPref); #FIXME: Doesn't work for text field, ref thing
+    $RequesterDefault = ($AuthorIDPref);
+  }
+}
+
+sub SetFileOptions {
+  my ($DocRevID) = @_;
+
+  if ($params{archive}) {
+    $Archive = $params{archive};
+  } else {
+    $Archive = $UploadTypePref
+  }  
+
+  if ($Archive eq "single") {$NumberUploads = 1;}  # Make sure
+  if ($Archive eq "multi")  {$Archive = "single";} # No real difference
+  if ($Archive ne "archive" && $Archive ne "single") {
+    $Archive = "single";
+  }  
+  
+  if ($params{numfile}) {               # User has selected
+    $NumberUploads = $params{numfile};
+  } elsif ($NumFilesPref && $mode ne "update") {             # User has a pref
+    if ($Meeting  || $OtherMeeting) {
+      if ($NumFilesPref < 3) {
+        $NumberUploads = 3;
+      } else {   
+        $NumberUploads = $NumFilesPref;
+      }  
+    } else {  
+      $NumberUploads = $NumFilesPref;
+    }   
+  } else {                              # No selection, no pref
+    if ($Meeting  || $OtherMeeting) {
+      $NumberUploads = 3;
+    } elsif ($mode eq "update") {
+      my @DocFiles = @{&FetchDocFiles($DocRevID)};
+      $NumberUploads = @DocFiles;
+      unless ($NumberUploads) { # Gyrations to handle docs that have 0 files
+        $NumberUploads = 1;
+      }  
+    } else {
+      $NumberUploads = 1;
+    }  
+  }
+}
+
 
 1;
