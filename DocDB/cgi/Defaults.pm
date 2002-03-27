46d5d5217890ac286fb912897281149c2380c382
vondo <>
2002-03-27 18:42:36 +0000

    Prefs for topic list and upload method now work

--- DocDB/cgi/Defaults.pm
+++ DocDB/cgi/Defaults.pm
@@ -8,4 +8,27 @@ sub SetAuthorMode {
     $AuthorMode = "list";
   }
 }
+
+sub SetTopicMode {
+  if ($params{topicmode}) {
+    $TopicMode = $params{topicmode};
+  } else {
+    $TopicMode = $TopicModePref;
+  }
+  if ($TopicMode ne "single" && $TopicMode ne "multi") {
+    $TopicMode = "multi";
+  }  
+}
+
+sub SetUploadMethod {
+  if ($params{upload}) {
+    $Upload = $params{upload};
+  } else {
+    $Upload = $UploadMethodPref;
+  }  
+  if ($Upload ne "http" && $Upload ne "file") {
+    $Upload = "file";
+  }  
+}
+
 1;
