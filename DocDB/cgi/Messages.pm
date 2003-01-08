9285fd3d4e3e38c60d7a5618645bd0192018ce3e
vondo <>
2003-01-08 17:00:17 +0000

    Cleanups, typos, and skeleton for MajorTopic administration

--- DocDB/cgi/Messages.pm
+++ DocDB/cgi/Messages.pm
@@ -10,7 +10,7 @@ $Msg_AdminNoConnect  = "Unable to connect to the database. Make sure you use the
 $Msg_AdminNoLogin    = "You must be logged in as the adminstrator to perform this action.";
 $Msg_DelInstWAuthors = "You can't delete institutions with authors. ".
                        "Delete the authors first if you want to delete the institution.";
-$Msg_DelInstEmpty    = "You mus select an institution to delete.";
+$Msg_ModInstEmpty    = "You must select an institution to modify.";
 
 # FIXME: Add more messages from other programs
 # FIXME: Add a localizable error message inclusion
