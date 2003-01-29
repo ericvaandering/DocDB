1199992c73e24bd044e831e03044834a76886ea3
vondo <>
2003-01-29 20:34:02 +0000

    Code to administer journals

--- DocDB/cgi/Messages.pm
+++ DocDB/cgi/Messages.pm
@@ -6,19 +6,21 @@
 #    Modified: 
 #
 
-$Msg_AdminNoConnect  = "Unable to connect to the database. Make sure you use the correct password.";
-$Msg_AdminNoLogin    = "You must be logged in as the adminstrator to perform this action.";
+$Msg_AdminNoConnect   = "Unable to connect to the database. Make sure you use the correct password.";
+$Msg_AdminNoLogin     = "You must be logged in as the adminstrator to perform this action.";
 
-$Msg_ModInstEmpty    = "You must select an institution to modify.";
-$Msg_DelInstWAuthors = "You can't delete institutions with authors. ".
-                       "Delete the authors first if you want to delete the institution.";
+$Msg_ModInstEmpty     = "You must select an institution to modify or delete.";
+$Msg_DelInstWAuthors  = "You can't delete institutions with authors. ".
+                        "Delete the authors first if you want to delete the institution.";
 
-$Msg_ModMajEmpty     = "You must select a major topic to modify.";
-$Msg_DelMajWTopics   = "You can't delete major topics with subtopics. ".
-                       "Delete the subtopics first if you want to delete the major topic.";
+$Msg_ModMajEmpty      = "You must select a major topic to modify.";
+$Msg_DelMajWTopics    = "You can't delete major topics with subtopics. ".
+                        "Delete the subtopics first if you want to delete the major topic.";
 
-$Msg_ModGroupEmpty   = "You must select a group to modify.";
-$Msg_ModGroupNone    = "The group you selected to modify does not exist.";
+$Msg_ModGroupEmpty    = "You must select a group to modify or delete.";
+$Msg_ModGroupNone     = "The group you selected to modify does not exist.";
+
+$Msg_ModJournaltEmpty = "You must select a journal to modify or delete.";
 
 # FIXME: Add more messages from other programs
 # FIXME: Add a localizable error message inclusion
