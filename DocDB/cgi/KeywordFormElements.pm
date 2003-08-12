e16eca73176a270710d4b57c0ee4fb0bc1fba140
garren <>
2003-08-12 14:35:43 +0000

    use truncated keyword descriptions

--- DocDB/cgi/KeywordFormElements.pm
+++ DocDB/cgi/KeywordFormElements.pm
@@ -26,7 +26,14 @@ sub KeywordSelectLong { # Scrolling selectable list for keywords, all info
   my %KeywordLabels = ();
   foreach my $ID (@KeywordListIDs) {
     ##$KeywordLabels{$ID} = $FullKeywords{$ID}." [$KeywordListEntries{$ID}{LONG}]"; 
-    $KeywordLabels{$ID} = $FullKeywords{$ID}; 
+    my $descr = $KeywordListEntries{$ID}{LONG};
+    my $long;
+    if ( (length $descr ) > 40 ) {
+      $long = substr($descr,0,40)." ...";
+    } else {
+      $long = $descr;
+    }
+    $KeywordLabels{$ID} = $FullKeywords{$ID}." [$long]"; 
   }
   print "<b><a ";
   &HelpLink("keywords");
