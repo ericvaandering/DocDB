b8798e375a5bb655005bcd7640413d271595dd00
ewv <>
2004-11-09 03:11:22 +0000

    other half of doctypes

--- DocDB/cgi/DocTypeHTML.pm
+++ DocDB/cgi/DocTypeHTML.pm
@@ -7,7 +7,17 @@
 #    Modified: 
 #
 
-sub DocTypeSelect { # Scrolling selectable list for doc type search
+sub DocTypeSelect (;%) { # Scrolling selectable list for doc type search
+  my (%Params) = @_;
+  
+  my $Disabled = $Params{-disabled}  || "0";
+  
+  my $Booleans = "";
+  
+  if ($Disabled) {
+    $Booleans .= "-disabled";
+  }  
+  
   my %DocTypeLabels = ();
   foreach my $DocTypeID (keys %DocumentTypes) {
     $DocTypeLabels{$DocTypeID} = "$DocumentTypes{$DocTypeID}{SHORT} [$DocumentTypes{$DocTypeID}{LONG}]";
@@ -16,26 +26,36 @@ sub DocTypeSelect { # Scrolling selectable list for doc type search
   &HelpLink("doctype");
   print "Document type:</a></b><br> \n";
   print $query -> scrolling_list(-size => 10, -name => "doctype", 
-                              -values => \%DocTypeLabels);
+                              -values => \%DocTypeLabels, $Booleans);
 };
 
 
-sub DocTypeEntryBox {
+sub DocTypeEntryBox (;%) {
+  my (%Params) = @_;
+  
+  my $Disabled = $Params{-disabled}  || "0";
+  
+  my $Booleans = "";
+  
+  if ($Disabled) {
+    $Booleans .= "-disabled";
+  }  
+  
   print "<table cellpadding=5><tr valign=top>\n";
   print "<td>\n";
   print "<b><a ";
   &HelpLink("doctypeentry");
   print "Short Description:</a></b><br> \n";
   print $query -> textfield (-name => 'name', 
-                             -size => 20, -maxlength => 32);
+                             -size => 20, -maxlength => 32, $Booleans);
   print "</td>\n";
   print "</tr><tr>\n";
   print "<td>\n";
   print "<b><a ";
   &HelpLink("doctypeentry");
   print "Long Description:</a></b><br> \n";
-  print $query -> textfield (-name => 'long', 
-                             -size => 40, -maxlength => 255);
+  print $query -> textfield (-name => 'longdesc', 
+                             -size => 40, -maxlength => 255, $Booleans);
   print "</td></tr>\n";
 
   print "</table>\n";
