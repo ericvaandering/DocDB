36e20467ebc81939b556f1d553182ea8fbae1d00
(no author) <>
2004-12-07 16:44:35 +0000

    This commit was manufactured by cvs2svn to create tag 'stable_sync_07_Dec_04'.
    Sprout from master 2004-12-07 16:44:34 UTC ewv 'Bring stable changes to head for further development'
    Delete:
        DocDB/doc/CHANGES
        DocDB/doc/CodeStandards.txt
        DocDB/doc/DocDB-license.html
        DocDB/doc/DocDBGlobals.html
        DocDB/doc/FAQ.html
        DocDB/doc/GPL.txt
        DocDB/doc/MigrationGuide
        DocDB/doc/TODO
        DocDB/doc/backup.html
        DocDB/doc/cpan.html
        DocDB/doc/createdb.html
        DocDB/doc/index-text.html
        DocDB/doc/index.shtml
        DocDB/doc/install-docdb.html
        DocDB/doc/my.cnf.html
        DocDB/doc/mysql.html
        DocDB/doc/mysqlModules.html
        DocDB/doc/notification.html
        DocDB/doc/resources.html
        DocDB/doc/rpms.html
        DocDB/doc/security.html
        DocDB/doc/setup-docdb.html
        DocDB/html/Public/OldDBTranslation.shtml
        DocDB/html/Restricted/Changelog.shtml
        DocDB/html/Restricted/DocDB_Help.shtml
        DocDB/html/Restricted/FAQ.shtml
        DocDB/html/Restricted/FileFormats.shtml
        DocDB/html/Restricted/MeetingOrganizer.shtml
        DocDB/html/Restricted/OldDBTranslation.shtml
        DocDB/html/Restricted/Scanning.shtml
        DocDB/html/Restricted/Scripts/DocumentAddForm.shtml
        DocDB/html/Restricted/htaccess.fnsimu1
        DocDB/scripts/CGIInclude.pm
        DocDB/scripts/ContentSearch.template
        DocDB/scripts/DeleteDocument
        DocDB/scripts/DocDBDaily.csh
        DocDB/scripts/DocDBHourly.csh
        DocDB/scripts/DocDBWeekly.csh
        DocDB/scripts/FullList
        DocDB/scripts/MigrateV3
        DocDB/scripts/MigrateV4
        DocDB/scripts/MigrateV5
        DocDB/scripts/MigrateV6
        DocDB/scripts/NotifyDigest
        DocDB/scripts/PublicInstall.csh
        DocDB/scripts/Resecure
        DocDB/scripts/SearchWrapper.template
        DocDB/scripts/TempleSecure
        DocDB/sql/CreateDatabase.SQL
        DocDB/sql/TestData.sql
        DocDB/sql/listTopics.sql

--- DocDB/sql/listTopics.sql
+++
@@ -1,6 +0,0 @@
-#
-# List major and minor topics with minor topic descriptions and sorted by major topic
-#
-select MajorTopic.ShortDescription, MinorTopic.ShortDescription, MinorTopic.LongDescription
-from MajorTopic, MinorTopic
-where MajorTopic.MajorTopicID = MinorTopic.MajorTopicID ;
