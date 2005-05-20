b9c81a6a7bd98ce55c5e60d594cba1a95ce6dfe6
ewv <>
2005-05-20 21:50:25 +0000

    Adding prototype in eclipse

--- DocDB/cgi/NotificationSQL.pm
+++ DocDB/cgi/NotificationSQL.pm
@@ -15,7 +15,7 @@
 #    along with DocDB; if not, write to the Free Software
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-sub GetEmailUserIDs {
+sub GetEmailUserIDs () {
   my $EmailUserID;
   my @EmailUserIDs;
 
