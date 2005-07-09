1704aebd79f3f762d92add4959cb1c89625c6da8
ewv <>
2005-07-09 18:41:58 +0000

    Removing unused subroutines

--- DocDB/cgi/Scripts.pm
+++ DocDB/cgi/Scripts.pm
@@ -16,35 +16,10 @@
 #    along with DocDB; if not, write to the Free Software
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-
-sub HelpPopupScript {
-  print "<script language=\"JavaScript\" type=\"text/javascript\">\n";
-  print "<!-- \n";
-
-  print "function helppopupwindow(page){\n";
-  print "window.open(page,\"docdbhelp\",\"width=450,height=350,menubar=0,resizable=1,scrollbars=1,status=0,titlebar=0,toolbar=0,left=0,top=0\");\n";
-  print "}\n";
-
-  print "//-->\n";
-  print "</script>\n";
-}
-
 sub HelpLink { # Change this, change FormElementTitle
                # Eventually, replace with  FormElementTitle
   my ($helpterm) = @_;
   print " style=\"color: red\" href=\"Javascript:helppopupwindow(\'$DocDBHelp?term=$helpterm\');\">";
-}
-
-sub TalkNotePopupScript {
-  print "<script language=\"JavaScript\" type=\"text/javascript\">\n";
-  print "<!-- \n";
-
-  print "function notepopupwindow(page){\n";
-  print "window.open(page,\"docdbnote\",\"width=800,height=350,menubar=0,resizable=1,scrollbars=1,status=0,titlebar=0,toolbar=0,left=0,top=0\");\n";
-  print "}\n";
-
-  print "//-->\n";
-  print "</script>\n";
 }
 
 sub TalkNoteLink {
@@ -52,33 +27,8 @@ sub TalkNoteLink {
   return "<a href=\"Javascript:notepopupwindow(\'$ShowTalkNote?sessiontalkid=$SessionTalkID\');\">Note</a>";
 }
 
-sub GroupLimitPopupScript {
-  print "<script language=\"JavaScript\" type=\"text/javascript\">\n";
-  print "<!-- \n";
-
-  print "function grouplimitpopupwindow(page){\n";
-  print "window.open(page,\"grouplimit\",\"width=450,height=350,menubar=0,resizable=1,scrollbars=1,status=0,titlebar=0,toolbar=0,left=0,top=0\");\n";
-  print "}\n";
-
-  print "//-->\n";
-  print "</script>\n";
-}
-
 sub GroupLimitLink {
   return "<a href=\"Javascript:grouplimitpopupwindow(\'$SelectGroups\');\">Limit Groups</a>";
-}
-
-sub ConfirmTalkPopupScript {
-  print "<script language=\"JavaScript\" type=\"text/javascript\">\n";
-  print "<!-- \n";
-
-  print "function confirmtalkpopupwindow(theForm){\n";
-  print "var oUrl=\"$ConfirmTalkHint?documentid=\"+theForm.documentid.value+\"&sessiontalkid=\"+theForm.sessiontalkid.value;\n";
-  print "window.open(oUrl,\"confirmtalk\",\"width=400,height=350,menubar=0,resizable=1,scrollbars=1,status=0,titlebar=0,toolbar=0,left=0,top=0\");\n";
-  print "}\n";
-
-  print "//-->\n";
-  print "</script>\n";
 }
 
 sub ConfirmTalkLink ($$) { #FIXME: Make onclick optional, use in DocumentTable
@@ -138,99 +88,6 @@ PREAMBLE
 
   print "//-->\n</script>\n";
 } 
-
-sub KeywordChooserPopupScript {
-  print "<script language=\"JavaScript\" type=\"text/javascript\">\n";
-  print "<!-- \n";
-
-  print "function keywordchooserwindow(page){\n";
-  print "window.open(page,\"KeywordChooser\",\"width=800,height=600,menubar=0,resizable=1,scrollbars=1,status=0,titlebar=0,toolbar=0,left=0,top=0\");\n";
-  print "}\n";
-
-  print "//-->\n";
-  print "</script>\n";
-}
-
-sub KeywordInsertScript {
-  
-  # Adapted from KochSuite, a German-authored Cookbook by 
-  # Michael Lestinsky <michael@zaphod.rhein-neckar.de>
-  
-  print <<ENDSCRIPT;
-
-  <script language="JavaScript1.2">
-  <!--
-    function InsertKeyword ( name ) {
-      if ( opener.document.forms[0].keywords.value == '' ) {
-        opener.document.forms[0].keywords.value = name;
-      } else {
-        opener.document.forms[0].keywords.value += ' ' + name;
-      }
-    }
-  //-->
-  </script>
-
-ENDSCRIPT
-
-}
-
-sub RevisionNoteInsertScript {
-  
-  # Adapted from KochSuite, a German-authored Cookbook by 
-  # Michael Lestinsky <michael@zaphod.rhein-neckar.de>
-  
-  print <<ENDSCRIPT;
-
-  <script language="JavaScript1.2">
-  <!--
-    function InsertRevisionNote ( note ) {
-      if ( document.forms[0].revisionnote.value == '' ) {
-        document.forms[0].revisionnote.value = note;
-      } else {
-        document.forms[0].revisionnote.value += note;
-      }
-    }
-  //-->
-  </script>
-
-ENDSCRIPT
-
-}
-
-sub SignatureInsertScript {
-  
-  # Adapted from KochSuite, a German-authored Cookbook by 
-  # Michael Lestinsky <michael@zaphod.rhein-neckar.de>
-  
-  print <<ENDSCRIPT;
-
-  <script language="JavaScript1.2">
-  <!--
-    function InsertSignature ( name ) {
-      if ( opener.document.forms[0].signofflist.value == '' ) {
-        opener.document.forms[0].signofflist.value = name;
-      } else {
-        opener.document.forms[0].signofflist.value += '\\n' + name;
-      }
-    }
-  //-->
-  </script>
-
-ENDSCRIPT
-
-}
-
-sub SignoffChooserPopupScript {
-  print "<script language=\"JavaScript\" type=\"text/javascript\">\n";
-  print "<!-- \n";
-
-  print "function signoffchooserwindow(page){\n";
-  print "window.open(page,\"SignoffChooser\",\"width=400,height=500,menubar=0,resizable=1,scrollbars=1,status=0,titlebar=0,toolbar=0,left=0,top=0\");\n";
-  print "}\n";
-
-  print "//-->\n";
-  print "</script>\n";
-}
 
 sub AdminDisableScripts (%) {
 
