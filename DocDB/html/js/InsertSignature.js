e7c0f19ebf30e584435fcd613a1a2b822af9b6d7
vondo <>
2005-07-11 20:56:07 +0000

    Really insert newline

--- DocDB/html/js/InsertSignature.js
+++ DocDB/html/js/InsertSignature.js
@@ -2,6 +2,6 @@ function InsertSignature (name) {
   if (opener.document.forms[0].signofflist.value == '') {
     opener.document.forms[0].signofflist.value = name;
   } else {
-    opener.document.forms[0].signofflist.value += '\\n' + name;
+    opener.document.forms[0].signofflist.value += '\n' + name;
   }
 }
