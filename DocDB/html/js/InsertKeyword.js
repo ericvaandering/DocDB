cf33b372784bde0709e0404ed2ff1cfdeec28f76
(no author) <>
2005-09-04 01:07:49 +0000

    This commit was manufactured by cvs2svn to create branch 'extxref'.
    Sprout from stable 2005-09-04 01:07:48 UTC vondo 'Remove debug'
    Delete:
        DocDB/doc/CHANGES
        DocDB/doc/CodeStandards.txt
        DocDB/doc/DocDB-license.html
        DocDB/doc/DocDBGlobals.html
        DocDB/doc/FAQ.html
        DocDB/doc/GPL.txt
        DocDB/doc/MigrationGuide
        DocDB/doc/RoadMap.html
        DocDB/doc/TODO
        DocDB/doc/XHTML_Validation.txt
        DocDB/doc/backup.html
        DocDB/doc/cacert.html
        DocDB/doc/certs.html
        DocDB/doc/certsrv.html
        DocDB/doc/cpan.html
        DocDB/doc/createdb.html
        DocDB/doc/doc_db.tex
        DocDB/doc/doesrvcert.html
        DocDB/doc/index-text.html
        DocDB/doc/index.shtml
        DocDB/doc/install-docdb.html
        DocDB/doc/my.cnf.html
        DocDB/doc/mysql.html
        DocDB/doc/mysqlModules.html
        DocDB/doc/mysqld.html
        DocDB/doc/notification.html
        DocDB/doc/problems.html
        DocDB/doc/resources.html
        DocDB/doc/rpms.html
        DocDB/doc/security.html
        DocDB/doc/setup-docdb.html
        DocDB/doc/sslconf.html
        DocDB/html/Public/OldDBTranslation.shtml
        DocDB/html/Restricted/Changelog.shtml
        DocDB/html/Restricted/DocDB_Help.shtml
        DocDB/html/Restricted/FAQ.shtml
        DocDB/html/Restricted/FileFormats.shtml
        DocDB/html/Restricted/MeetingOrganizer.shtml
        DocDB/html/Restricted/OldDBTranslation.shtml
        DocDB/html/Restricted/Scanning.shtml
        DocDB/html/Restricted/Scripts/DocumentAddForm.shtml
        DocDB/html/Restricted/htaccess.fnsimu1
        DocDB/html/css/DocDB.css
        DocDB/html/css/DocDBCertificateApplyForm.css
        DocDB/html/css/DocDBDisplayMeeting.css
        DocDB/html/css/DocDBDocDBInstructions.css
        DocDB/html/css/DocDBDocDBInstructions_IE.css
        DocDB/html/css/DocDBDocumentDatabase.css
        DocDB/html/css/DocDBMeetingModify.css
        DocDB/html/css/DocDBModifyHome.css
        DocDB/html/css/DocDBModifyHome_IE.css
        DocDB/html/css/DocDBSearchForm.css
        DocDB/html/css/DocDBSelectGroups.css
        DocDB/html/css/DocDBSessionModify.css
        DocDB/html/css/DocDBSetGroups.css
        DocDB/html/css/DocDBShowCalendar.css
        DocDB/html/css/DocDBStatistics.css
        DocDB/html/css/DocDB_IE.css
        DocDB/html/css/ProjectDocDB.css.example
        DocDB/html/js/AuthorAdminDisable.js
        DocDB/html/js/EventAdminDisable.js
        DocDB/html/js/EventChooser.js
        DocDB/html/js/GroupAdminDisable.js
        DocDB/html/js/InsertKeyword.js
        DocDB/html/js/InsertRevisionNote.js
        DocDB/html/js/InsertSignature.js
        DocDB/html/js/KeywordAdminDisable.js
        DocDB/html/js/MiscAdminDisable.js
        DocDB/html/js/PersonalAccountAdminDisable.js
        DocDB/html/js/PopUps.js
        DocDB/html/js/TopicAdminDisable.js
        DocDB/html/js/TopicChooser.js
        DocDB/scripts/BreakMeetings
        DocDB/scripts/CGIInclude.pm
        DocDB/scripts/ContentSearch.template
        DocDB/scripts/DeleteDocument
        DocDB/scripts/DocDBDaily.csh
        DocDB/scripts/DocDBHourly.csh
        DocDB/scripts/DocDBWeekly.csh
        DocDB/scripts/FullList
        DocDB/scripts/MigrateV3
        DocDB/scripts/MigrateV4
        DocDB/scripts/MigrateV5
        DocDB/scripts/MigrateV6
        DocDB/scripts/MigrateV7
        DocDB/scripts/NotifyDigest
        DocDB/scripts/PublicInstall.csh
        DocDB/scripts/Resecure
        DocDB/scripts/SearchWrapper.template
        DocDB/scripts/TempleSecure
        DocDB/sql/CreateDatabase.SQL
        DocDB/sql/TestData.sql
        DocDB/sql/listTopics.sql

--- DocDB/html/js/InsertKeyword.js
+++
@@ -1,7 +0,0 @@
-function InsertKeyword (name) {
-  if (opener.document.forms[0].keywords.value == '') {
-    opener.document.forms[0].keywords.value = name;
-  } else {
-    opener.document.forms[0].keywords.value += ' ' + name;
-  }
-}
