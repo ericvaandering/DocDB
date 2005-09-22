7910b85a0849a3f66f5846f72bafff0a01cb629a
ewv <>
2005-09-22 16:52:47 +0000

    Try to avoid JS namespace?

--- DocDB/html/js/ExternalDocDBAdminDisable.js
+++ DocDB/html/js/ExternalDocDBAdminDisable.js
@@ -3,21 +3,21 @@ function disabler_externaldocdb() {
    document.externaldocdb.externaldocdbs.disabled = true;
    document.externaldocdb.project.disabled        = false;
    document.externaldocdb.desc.disabled           = false;
-   document.externaldocdb.public.disabled         = false;
-   document.externaldocdb.private.disabled        = false;
+   document.externaldocdb.puburl.disabled         = false;
+   document.externaldocdb.privurl.disabled        = false;
  }
  if (document.externaldocdb.admaction[1].checked == true) {
    document.externaldocdb.externaldocdbs.disabled = false;
    document.externaldocdb.project.disabled        = true;
    document.externaldocdb.desc.disabled           = true;
-   document.externaldocdb.public.disabled         = true;
-   document.externaldocdb.private.disabled        = true;
+   document.externaldocdb.puburl.disabled         = true;
+   document.externaldocdb.privurl.disabled        = true;
  }
  if (document.externaldocdb.admaction[2].checked == true) {
    document.externaldocdb.externaldocdbs.disabled = false;
    document.externaldocdb.project.disabled        = false;
    document.externaldocdb.desc.disabled           = false;
-   document.externaldocdb.public.disabled         = false;
-   document.externaldocdb.private.disabled        = false;
+   document.externaldocdb.puburl.disabled         = false;
+   document.externaldocdb.privurl.disabled        = false;
  }
 }
