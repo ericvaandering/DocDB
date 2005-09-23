4f9653df3b23762ac111e5b43f3ad54ef5653c76
ewv <>
2005-09-23 09:30:43 +0000

    Redo headers, meeting -> event, and remove some references to old style

--- DocDB/cgi/EventInstructions.pm
+++ DocDB/cgi/EventInstructions.pm
@@ -1,4 +1,4 @@
-# Description: The instructions for the meeting organizer and calendar in DocDB. 
+# Description: The instructions for the event organizer and calendar in DocDB. 
 #              This is mostly HTML, but making  it a script allows us to eliminate
 #              parts of it that we don't want and get it following everyone's
 #              style, and allows groups to add to it with ProjectMessages.
@@ -50,15 +50,15 @@ TOC
 
 sub EventInstructionsBody {
   print <<HTML;
-<h1>A. Introduction</h1>
+<h1>Introduction</h1>
 
 These instructions refer to a "moderator," but this is just any user with any
-valid password who is organizing a meeting. Several people can collaborate to
-organize a meeting, but where changes collide or appear to collide, only the
+valid password who is organizing an event. Several people can collaborate to
+organize an event, but where changes collide or appear to collide, only the
 first is taken.
 <p/>
 
-The meeting organizer system provides the ability to set up meetings with
+The event organizer system provides the ability to set up events with
 arbitrary numbers of sessions and breaks and order these sessions and breaks.
 Within each session, a moderator can set up an arbitrary number of talks and
 small breaks, discussion sessions, etc. These entries are also ordered. Each
@@ -68,46 +68,46 @@ agenda. Anything from an afternoon video conference to conferences with
 parallel and plenary sessions can be organized with this organizer.
 
 <a name="create"/>
-<h1>B. Creating a New Meeting</h1>
+<h1>Creating a New Event</h1>
 
-The first step in creating a meeting is to set up the sessions in the meeting.
-You begin this process by clicking on "Oragnize a new meeting" from the DocDB
-modifications page. Meetings can be
-created as a subtopic in the DocDB or, for smaller meetings (perhaps by small
+The first step in creating an event is to set up the sessions in the event.
+You begin this process by clicking on "Oragnize a new event" from the DocDB
+modifications page. Events can be
+created as a subtopic in the DocDB or, for smaller events (perhaps by small
 subgroups of people), not as a subtopic. Ask an administrator if you are unsure
 if you should create a topic or not.
 Whether the moderator chooses to create
-a topic or not, all the functionality of the meeting organizer is still
-available. A list of the topics which are designated as "meeting-like" are
-shown; simply select one of these to create the subtopic.<p/>
+a topic or not, all the functionality of the event organizer is still
+available. A list of the groups of events are
+shown; simply select one.<p/>
 
 The "Show All Talks" selection controls what the user sees when viewing a
-meeting.  In the meeting view, either all the sessions for a meeting with all
+event.  In the event view, either all the sessions for an event with all
 their talks can be shown or just links to the
-various sessions. This should probably be checked for meetings with just a few
-dozen talks, but left unchecked for larger meetings.<p/>
+various sessions. This should probably be checked for events with just a few
+dozen talks, but left unchecked for larger events.<p/>
 
-Space is provided to provide both long and short descriptions of the meeting.
-If the meeting is being created with a subtopic, both of these descriptions are
+Space is provided to provide both long and short descriptions of the event.
+If the event is being created with a subtopic, both of these descriptions are
 used. If a subtopic is not being created, only the long description is used. Also
-provided are spaces to specify a location and URL for the conference or meeting
+provided are spaces to specify a location and URL for the conference or event
 along with the start and end dates.<p/>
 
-The boxes labeled Meeting Preamble and Epilogue provide a space for text which
+The boxes labeled Event Preamble and Epilogue provide a space for text which
 will be placed at the top and bottom respectively of the display of the
-meeting. A welcome message or instructions can be placed here.<p/>
+event. A welcome message or instructions can be placed here.<p/>
 
 Finally, the View and Modify selections are used to control which groups may
-view and modify the agenda for the meetings. The documents (talks) themselves
+view and modify the agenda for the events. The documents (talks) themselves
 rely on their own security, not this setting.<p/>
 
-A nearly identical form is used for modifying meeting information.  
+A nearly identical form is used for modifying event information.  
 
 <a name="sessions"/>
-<h1>C. Creating Sessions</h1>
+<h1>Creating Sessions</h1>
 
-On the same form used for creating or modifying a meeting, the moderator is able
-to set up one or more sessions in the meeting. If there are not enough spaces
+On the same form used for creating or modifying an event, the moderator is able
+to set up one or more sessions in the event. If there are not enough spaces
 for all the needed sessions, don't worry; blank slots will be added when
 "Submit" is pressed.<p/>
 
@@ -125,15 +125,15 @@ For each session or break, a location (such as a room number) and starting time 
 session title should be entered and a longer description of the session
 (such as an explanation of the topics covered) may also be entered.<p/>
 
-Once at least one session has been added to the meeting, talks can be associated
+Once at least one session has been added to the event, talks can be associated
 with the sessions.<p/>
 
 <a name="talks"/>
-<h1>D. Adding and Modifying Talks in a Session</h1>
+<h1>Adding and Modifying Talks in a Session</h1>
 
 To create or modify slots for talks in a session, either click on the "Modify Session" link
 in the navigation bar on the "Display Session" page or the "Modify Talks" link
-on the "Modify Meeting" page. This allows the moderator to add as many talks or
+on the "Modify Event" page. This allows the moderator to add as many talks or
 breaks as needed. In this context, breaks can be announcements, discussions,
 or coffee breaks during a session (any activity which won't have a document
 attached to it).<p/>
@@ -144,14 +144,14 @@ usually several open slots for talks, blank ones are added when "Submit" is
 pressed.
 
 <a name="basicinfo"/>
-<h2>1. Entering basic talk information</h2>
+<h2>Entering basic talk information</h2>
 
 For each talk, at least a suggested title and time (length) should be entered.
 A note on each talk can also be entered, but these are only visible when
-clicking on the "Note" link in the meeting or session displays. 
+clicking on the "Note" link in the event or session displays. 
 
 <a name="order"/>
-<h2>2. Ordering the talks</h2>
+<h2>Ordering the talks</h2>
 
 On the far left is the document order within the session. To reorder talks
 within the session, just input new numbers and press "Submit." Decimal numbers
@@ -159,12 +159,12 @@ are allowed, so entering "1.5" will place that talk between the talks currently
 numbered "1" and "2." 
 
 <a name="confirm"/>
-<h2>3. Specifying, deleting, and confirming documents</h2>
+<h2>Specifying, deleting, and confirming documents</h2>
 
 On the form there are places to enter the document number of a talk if the
 moderator already knows it, to confirm a suggestion by the DocDB, and to delete
 the entry for a talk entirely. (This will NOT delete the document from the
-database, just the entry for the meeting.)<p/>
+database, just the entry for the event.)<p/>
 
 A confirmed talk is one where the relationship between agenda entry and document
 has been verified by a human, not guessed at by DocDB as explained below.
@@ -174,7 +174,7 @@ If document numbers are entered manually, the "Confirm" box(es) must also be
 checked, or DocDB will guess its own numbers instead.
 
 <a name="hints"/>
-<h2>4. Giving hints about the talks</h2>
+<h2>Giving hints about the talks</h2>
 
 Finally, the moderator may enter the suggested authors and topics for the talks
 to be given. This has two purposes. First, before documents are entered into
@@ -182,7 +182,7 @@ the DocDB, attendees can more clearly see what the proposed agenda is.
 Secondly, this assists DocDB in finding the correct matches as described below.
 
 <a name="matching"/>
-<h1>E. How the DocDB matches your entries with documents</h1>
+<h1>How the DocDB matches your entries with documents</h1>
 
 In addition to the moderator associating talks as described above, there are
 two other ways this can happen. The first way is to have the user themselves
@@ -196,16 +196,16 @@ documents confirmed for another agenda entry to a second entry, so confirming
 documents and letting it guess again will often find more matches. 
 
 <a name="userentry"/>
-<h2>1. User selects the talk</h2>
+<h2>User selects the talk</h2>
 
-When a user follows the link at the top of a session or meeting display that says "Add a talk to this
-meeting of conference," they will see a document entry form with one small addition: a menu to select
-his or her talk from the list of talks for that meeting or session that have not yet been entered.
+When a user follows the link at the top of a session or event display that says "Add a talk to this
+event of conference," they will see a document entry form with one small addition: a menu to select
+his or her talk from the list of talks for that event or session that have not yet been entered.
 When the user selects his or her talk from this list, it is entered into the agenda as a confirmed
 talk, just as if the moderator had followed the instructions below.
 
 <a name="hinting"/>
-<h2>2. DocDB selects based on hints</h2>
+<h2>DocDB selects based on hints</h2>
 
 For entries without a confirmed document, the DocDB will try to figure out which agenda entry matches
 which document. To do this, the DocDB constructs a list of documents which
@@ -215,21 +215,21 @@ document among them. This document becomes an unconfirmed match with the entry
 in the agenda. If it guesses right, confirm the document as discussed below.<p/>
 
 The list of documents to check against comes from two sources. First documents
-with modification times in a time window around the meeting dates are
-considered. Second, if the meeting is associated with a topic (discussed
+with modification times in a time window around the event dates are
+considered. Second, if the event is associated with a topic (discussed
 above), documents associated with that topic are considered.<p/>
 
 Documents are matched with the agenda entries using a scoring system that takes
 into account several criteria:
 <ul>
-<li>Whether the document is associated with the meeting topic</li>
+<li>Whether the document is associated with the event topic</li>
 <li>If the document's topic(s) match those in the agenda</li>
 <li>If the document's author(s) match those in the agenda</li>
 <li>How well the title of the document matches the suggested title in the
 agenda</li>
 </ul>
  
-Documents that are associated with the meeting topic receive a bonus over
+Documents that are associated with the event receive a bonus over
 documents that just happen to fall in the time window. Points are also assigned
 to documents for each author or topic that matches those suggested in the
 agenda and for matching words in the titles. For each agenda entry/document
@@ -240,18 +240,21 @@ consideration, which may change which assignments DocDB makes.<p/>
 The precise algorithm used in choosing the best match can be determined by
 looking at the DocDB code. <p/>
 
+<a name="reserve"/>
+<h2>Moderator reserves initial documents</h2>
+
 <a name="confirm2"/>
-<h2>3. Moderator corrects and/or confirms</h2>
+<h2>Moderator corrects and/or confirms</h2>
 
 Finally, a moderator can manually enter document numbers and check "Confirm" or
-just check "Confirm" to verify DocDB's choices. When managing a meeting, it is
+just check "Confirm" to verify DocDB's choices. When managing an event, it is
 a good idea to periodically confirm unconfirmed documents for two reasons.
 First, the DocDB might find a better match which is <b>not</b> correct. Second,
 confirming documents removes them from the list of considered documents often
 allowing the DocDB to find correct matches for remaining entries in the
 agenda.<p/>
 
-For very small meetings (just a few talks) moderators may wish to not use hints
+For very small events (just a few talks) moderators may wish to not use hints
 at all and just manually enter the talks.<p/>
 
 HTML
