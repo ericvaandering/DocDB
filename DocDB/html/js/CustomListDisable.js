f10f2d237ebcfa014daa5a2b64e4c3b36e54848b
vondo <>
2005-12-28 04:00:23 +0000

    Try to get JS working

--- DocDB/html/js/CustomListDisable.js
+++ DocDB/html/js/CustomListDisable.js
@@ -1,16 +1,16 @@
 function disabler_customlist(AllowEvent,AllowAll) {
-  document.event.events.disabled      = true;
-  document.event.eventgroups.disabled = true;
-  document.event.events.topics        = true;
-  document.event.events.defaultlists  = true;
+  document.customlist.events.disabled      = true;
+  document.customlist.eventgroups.disabled = true;
+  document.customlist.events.topics        = true;
+  document.customlist.events.defaultlists  = true;
   if (AllowAll || document.event.scope[1].checked == true) {
-    document.event.events.disabled      = false;
-    document.event.eventgroups.disabled = false;
-    document.event.events.topics        = false;
-    document.event.events.defaultlists  = false;
+    document.customlist.events.disabled      = false;
+    document.customlist.eventgroups.disabled = false;
+    document.customlist.events.topics        = false;
+    document.customlist.events.defaultlists  = false;
   }
   if (AllowEvent) {
-    document.event.events.disabled      = false;
+    document.customlist.events.disabled      = false;
   } 
 }
 
