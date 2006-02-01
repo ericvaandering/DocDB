33743ba1d2e66b3e824356270a22053ea393d363
vondo <>
2006-02-01 16:18:51 +0000

    DocTypes

--- DocDB/html/js/CustomListDisable.js
+++ DocDB/html/js/CustomListDisable.js
@@ -3,10 +3,13 @@ function disabler_customlist(AllowEvent,AllowAll) {
   document.customlist.eventgroups.disabled  = true;
   document.customlist.topics.disabled       = true;
   document.customlist.defaultlists.disabled = true;
+  document.customlist.doctype.disabled      = true;
+  
   if (AllowAll || document.customlist.scope[1].checked == true) {
     document.customlist.events.disabled       = false;
     document.customlist.eventgroups.disabled  = false;
     document.customlist.topics.disabled       = false;
+    document.customlist.doctype.disabled      = false;
     document.customlist.defaultlists.disabled = false;
   }
   if (AllowEvent) {
