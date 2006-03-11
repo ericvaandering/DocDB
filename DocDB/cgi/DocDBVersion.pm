f4175937ec5ccfe9cb13f2af3fb82974bd101d96
vondo <>
2006-03-11 20:58:47 +0000

    bump version

--- DocDB/cgi/DocDBVersion.pm
+++ DocDB/cgi/DocDBVersion.pm
@@ -15,6 +15,6 @@
 #    along with DocDB; if not, write to the Free Software
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-$DocDBVersion = "7.3.0"; # Add Custom lists
+$DocDBVersion = "7.3.1"; # Add Custom lists
 
 1;
