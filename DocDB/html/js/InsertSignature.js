0d8a0f2976b530f4cec78c0ebe6073ce1ff0e7bc
(no author) <>
2006-05-08 16:54:08 +0000

    This commit was manufactured by cvs2svn to create branch 'v751e'.
    Sprout from stable 2006-05-08 16:54:07 UTC vondo 'Merging in v751(a,b,c)'
    Delete:
        DocDB/doc/CHANGES
        DocDB/doc/CodeStandards.txt
        DocDB/doc/DocDB-license.html
        DocDB/doc/DocDBGlobals.html
        DocDB/doc/FAQ.html
        DocDB/doc/GPL.txt
        DocDB/doc/GetDocDB.txt
        DocDB/doc/MigrationGuide
        DocDB/doc/RoadMap.html
        DocDB/doc/SFHome.html
        DocDB/doc/TODO
        DocDB/doc/XHTML_Validation.txt
        DocDB/doc/advanced-ProjectGlobals.html
        DocDB/doc/backup.html
        DocDB/doc/cacert.html
        DocDB/doc/certs.html
        DocDB/doc/certsrv.html
        DocDB/doc/cpan.html
        DocDB/doc/createdb.html
        DocDB/doc/doc_db.tex
        DocDB/doc/doesrvcert.html
        DocDB/doc/index-text.html
        DocDB/doc/index.shtml
        DocDB/doc/install-docdb.html
        DocDB/doc/migratev6v7.html
        DocDB/doc/my.cnf.html
        DocDB/doc/mysql.html
        DocDB/doc/mysqlModules.html
        DocDB/doc/mysqld.html
        DocDB/doc/notification.html
        DocDB/doc/problems.html
        DocDB/doc/resources.html
        DocDB/doc/rpms.html
        DocDB/doc/security.html
        DocDB/doc/setup-docdb.html
        DocDB/doc/sslconf.html
        DocDB/html/Public/OldDBTranslation.shtml
        DocDB/html/Restricted/Scripts/DocumentAddForm.shtml
        DocDB/html/css/DocDB.css
        DocDB/html/css/DocDBCertificateApplyForm.css
        DocDB/html/css/DocDBDisplayMeeting.css
        DocDB/html/css/DocDBDocDBInstructions.css
        DocDB/html/css/DocDBDocDBInstructions_IE.css
        DocDB/html/css/DocDBDocumentDatabase.css
        DocDB/html/css/DocDBMeetingModify.css
        DocDB/html/css/DocDBModifyHome.css
        DocDB/html/css/DocDBModifyHome_IE.css
        DocDB/html/css/DocDBSearchForm.css
        DocDB/html/css/DocDBSelectGroups.css
        DocDB/html/css/DocDBSessionModify.css
        DocDB/html/css/DocDBSetGroups.css
        DocDB/html/css/DocDBShowCalendar.css
        DocDB/html/css/DocDBStatistics.css
        DocDB/html/css/DocDB_IE.css
        DocDB/html/css/ProjectDocDB.css.example
        DocDB/html/js/AuthorAdminDisable.js
        DocDB/html/js/CustomListDisable.js
        DocDB/html/js/EventAdminDisable.js
        DocDB/html/js/EventChooser.js
        DocDB/html/js/ExternalDocDBAdminDisable.js
        DocDB/html/js/GroupAdminDisable.js
        DocDB/html/js/InsertKeyword.js
        DocDB/html/js/InsertRevisionNote.js
        DocDB/html/js/InsertSignature.js
        DocDB/html/js/KeywordAdminDisable.js
        DocDB/html/js/MiscAdminDisable.js
        DocDB/html/js/PersonalAccountAdminDisable.js
        DocDB/html/js/PopUps.js
        DocDB/html/js/TopicAdminDisable.js
        DocDB/html/js/TopicChooser.js
        DocDB/scripts/CGIInclude.pm
        DocDB/scripts/CheckModules
        DocDB/scripts/ContentSearch.template
        DocDB/scripts/DeleteDocument
        DocDB/scripts/DocDBDaily.csh
        DocDB/scripts/DocDBHourly.csh
        DocDB/scripts/DocDBWeekly.csh
        DocDB/scripts/FullList
        DocDB/scripts/LinkCheck
        DocDB/scripts/MigrateV3
        DocDB/scripts/MigrateV4
        DocDB/scripts/MigrateV5
        DocDB/scripts/MigrateV6
        DocDB/scripts/MigrateV7
        DocDB/scripts/NotifyDigest
        DocDB/scripts/PublicInstall.csh
        DocDB/scripts/Resecure
        DocDB/scripts/ScriptInstall
        DocDB/scripts/SearchWrapper.template
        DocDB/scripts/examples/AddEventDocuments
        DocDB/scripts/examples/BreakMeetings
        DocDB/scripts/examples/README
        DocDB/scripts/examples/Secure
        DocDB/scripts/examples/TempleSecure
        DocDB/sql/CreateDatabase.SQL
        DocDB/sql/TestData.sql
        DocDB/sql/listTopics.sql

--- DocDB/html/js/InsertSignature.js
+++
@@ -1,7 +0,0 @@
-function InsertSignature (name) {
-  if (opener.document.forms[0].signofflist.value == '') {
-    opener.document.forms[0].signofflist.value = name;
-  } else {
-    opener.document.forms[0].signofflist.value += '\\n' + name;
-  }
-}
