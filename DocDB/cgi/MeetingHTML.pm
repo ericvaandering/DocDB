80d1268d8a232ab2b17b4fa8a04a3ad27428575d
vondo <>
2006-07-04 18:56:48 +0000

    Try right sidebar

--- DocDB/cgi/MeetingHTML.pm
+++ DocDB/cgi/MeetingHTML.pm
@@ -501,7 +501,7 @@ sub PrintEventLeftSidebar ($) {
   print "</div>\n"; # UpdateButtons
 }
 
-print PrintEventRightSideBar ($) {
+sub PrintEventRightSideBar ($) {
   my ($ArgRef) = @_;
   my $EventID     = exists $ArgRef->{-eventid}     ? $ArgRef->{-eventid}     : 0;
   my $SessionID   = exists $ArgRef->{-sessionid}   ? $ArgRef->{-sessionid}   : 0;
