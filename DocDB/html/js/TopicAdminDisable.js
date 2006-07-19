e44f7160f58198749541eccaa1ff4ccc7c269305
vondo <>
2006-07-19 00:42:42 +0000

    Javascript

--- DocDB/html/js/TopicAdminDisable.js
+++ DocDB/html/js/TopicAdminDisable.js
@@ -1,38 +1,20 @@
-function disabler_minortopic() {
-  if (document.minortopic.admaction[0].checked == true) {
-    document.minortopic.topics.disabled = true;
-    document.minortopic.shortdesc.disabled = false;
-    document.minortopic.longdesc.disabled = false;
-    document.minortopic.majortopic.disabled = false;
+function disabler_topic() {
+  if (document.topic.admaction[0].checked == true) {
+    document.topic.topics.disabled      = true;
+    document.topic.shortdesc.disabled   = false;
+    document.topic.longdesc.disabled    = false;
+    document.topic.parenttopic.disabled = false;
   }
-  if (document.minortopic.admaction[1].checked == true) {
-    document.minortopic.topics.disabled = false;
-    document.minortopic.shortdesc.disabled = true;
-    document.minortopic.longdesc.disabled = true;
-    document.minortopic.majortopic.disabled = true;
+  if (document.topic.admaction[1].checked == true) {
+    document.topic.topics.disabled      = false;
+    document.topic.shortdesc.disabled   = true;
+    document.topic.longdesc.disabled    = true;
+    document.topic.parenttopic.disabled = true;
   }
-  if (document.minortopic.admaction[2].checked == true) {
-    document.minortopic.topics.disabled = false;
-    document.minortopic.shortdesc.disabled = false;
-    document.minortopic.longdesc.disabled = false;
-    document.minortopic.majortopic.disabled = false;
+  if (document.topic.admaction[2].checked == true) {
+    document.topic.topics.disabled      = false;
+    document.topic.shortdesc.disabled   = false;
+    document.topic.longdesc.disabled    = false;
+    document.topic.parenttopic.disabled = false;
   }
-}
-
-function disabler_majortopic() {
- if (document.majortopic.admaction[0].checked == true) {
-   document.majortopic.shortdesc.disabled = false;
-   document.majortopic.longdesc.disabled = false;
-   document.majortopic.majortopic.disabled = true;
- }
- if (document.majortopic.admaction[1].checked == true) {
-   document.majortopic.shortdesc.disabled = true;
-   document.majortopic.longdesc.disabled = true;
-   document.majortopic.majortopic.disabled = false;
- }
- if (document.majortopic.admaction[2].checked == true) {
-   document.majortopic.shortdesc.disabled = false;
-   document.majortopic.longdesc.disabled = false;
-   document.majortopic.majortopic.disabled = false;
- }
 }
