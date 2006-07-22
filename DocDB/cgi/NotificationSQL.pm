47163ed97a373ac42112eacd9a708abd863fab80
vondo <>
2006-07-22 02:03:14 +0000

    Complete topic notifications?

--- DocDB/cgi/NotificationSQL.pm
+++ DocDB/cgi/NotificationSQL.pm
@@ -78,7 +78,7 @@ sub DeleteNotifications ($) {
   my $EmailUserID = exists $ArgRef->{-emailuserid} ? $ArgRef->{-emailuserid} : 0;
   
   if ($EmailUserID) { # FIXME: Don't do document specific?
-    my $Delete = $dbh -> prepare("delete from Notification where Type<>'Document' EmailUserID=?");
+    my $Delete = $dbh -> prepare("delete from Notification where Type<>'Document' and EmailUserID=?");
     $Delete -> execute($EmailUserID);
   }
   
