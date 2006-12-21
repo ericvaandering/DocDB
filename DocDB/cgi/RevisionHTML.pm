4c72c375da8ac0bc50cd332f5009acba7d79beaf
vondo <>
2006-12-21 21:01:52 +0000

    Lynn, try this

--- DocDB/cgi/RevisionHTML.pm
+++ DocDB/cgi/RevisionHTML.pm
@@ -198,7 +198,7 @@ sub PrintRevisionInfo {
 
   PrintAbstract($DocRevisions{$DocRevID}{Abstract}); # All are called only here, so changes are OK
   FileListByRevID($DocRevID); # All are called only here, so changes are OK
-  TopicListByID( {-topicids => \@TopicIDs, -listelement => "long"} );
+  print TopicListByID( {-topicids => \@TopicIDs, -listelement => "long"} );
   AuthorListByAuthorRevID({ -authorrevids => \@RevAuthorIDs });
   PrintKeywords($DocRevisions{$DocRevID}{Keywords});
   PrintRevisionNote($DocRevisions{$DocRevID}{Note});
