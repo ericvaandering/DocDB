f67ee1c6cf671033edd98b8e586bbaf040b55819
vondo <>
2007-02-17 17:03:06 +0000

    Add HTML/XML output

--- DocDB/cgi/DocDBVersion.pm
+++ DocDB/cgi/DocDBVersion.pm
@@ -15,6 +15,6 @@
 #    along with DocDB; if not, write to the Free Software
 #    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-$DocDBVersion = "8.2.5";
+$DocDBVersion = "8.4.0";
 
 1;
