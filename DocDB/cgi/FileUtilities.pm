6a4d5bd05ddc0b09451fb33a6f3241bbd8a5ee3e
vondo <>
2007-02-18 19:09:08 +0000

    Can we upload files?

--- DocDB/cgi/FileUtilities.pm
+++ DocDB/cgi/FileUtilities.pm
@@ -4,7 +4,7 @@
 #    This file is part of DocDB.
 
 #    DocDB is free software; you can redistribute it and/or modify
-#    it under the terms of version 2 of the GNU General Public License 
+#    it under the terms of version 2 of the GNU General Public License
 #    as published by the Free Software Foundation.
 
 #    DocDB is distributed in the hope that it will be useful,
@@ -25,38 +25,38 @@
 #  Description -- Description of the file
 #  Main        -- Boolean, is it a "main" file
 # These settings apply only to URL uploads
-#  URL         -- URL of the file  
-#  Pass        -- Password for wget 
-#  User        -- Username for wget 
+#  URL         -- URL of the file
+#  Pass        -- Password for wget
+#  User        -- Username for wget
 #  NewFilename -- New name of files (wget -O option)
 
 sub AddFiles (%) {
   require "FileSQL.pm";
   require "FSUtilities.pm";
-  
+
   my %Params = @_;
-  
-  my $DocRevID   = $Params{-docrevid};   
-  my $DateTime   = $Params{-datetime}; 
-  my $ReplaceOld = $Params{-replaceold}; # Replace files of the same name  
-  my $OldVersion = $Params{-oldversion}; # For copying files from old version  
+
+  my $DocRevID   = $Params{-docrevid};
+  my $DateTime   = $Params{-datetime};
+  my $ReplaceOld = $Params{-replaceold}; # Replace files of the same name
+  my $OldVersion = $Params{-oldversion}; # For copying files from old version
 
   my %Files = %{$Params{-files}};
-  
+
   push @DebugStack,"Adding files for DRI $DocRevID";
-  
+
   my @FileIDs = (); my $FileID;
   unless ($DocRevID) {
     return @FileIDs;
-  }  
+  }
 
   my @Files = sort keys %Files;
-  
+
   &FetchDocRevisionByID($DocRevID);
   my $Version    = $DocRevisions{$DocRevID}{Version};
   my $DocumentID = $DocRevisions{$DocRevID}{DOCID};
-  &MakeDirectory($DocumentID,$Version); 
-  my $Directory = &GetDirectory($DocumentID,$Version); 
+  &MakeDirectory($DocumentID,$Version);
+  my $Directory = &GetDirectory($DocumentID,$Version);
 
   foreach my $File (@Files) {
     my $ShortName = "";
@@ -77,47 +77,48 @@ sub AddFiles (%) {
       &FetchFile($OldFileID);
       $ShortName = $DocFiles{$OldFileID}{Name};
       $DateTime  = $DocFiles{$OldFileID}{Date};
-    } # else other methods
-    
+    } else { # else other methods
+      push @DebugStack,"Don't understand method to insert file $Files{$File}{Name}";
+    }
     if ($ReplaceOld && $ShortName) {
       my $OldFileID = &ExistsFile($DocRevID,$ShortName);
       &DeleteFile(-fileid => $OldFileID);
     }
-      
+
     if ($ShortName) {
       $FileID = &InsertFile(-docrevid    => $DocRevID, -datetime => $DateTime,
                             -filename    => $ShortName,
                             -main        => $Files{$File}{Main},
                             -description => $Files{$File}{Description});
       push @FileIDs,$FileID;
-    }  
-  } 
-  return @FileIDs;   
+    }
+  }
+  return @FileIDs;
 }
 
 sub AddArchive (%) {
   require "FileSQL.pm";
   require "FSUtilities.pm";
-  
+
   my %Params = @_;
-  
-  my $DocRevID = $Params{-docrevid};   
-  my $DateTime = $Params{-datetime}; 
+
+  my $DocRevID = $Params{-docrevid};
+  my $DateTime = $Params{-datetime};
 
   my %Archive = %{$Params{-archive}};
 
   push @DebugStack,"Adding archive for DRI $DocRevID";
-  
+
   my @FileIDs = (); my $FileID;
   unless ($DocRevID) {
     return @FileIDs;
-  }  
+  }
 
   &FetchDocRevisionByID($DocRevID);
   my $Version    = $DocRevisions{$DocRevID}{Version};
   my $DocumentID = $DocRevisions{$DocRevID}{DOCID};
-  &MakeDirectory($DocumentID,$Version); 
-  my $Directory = &GetDirectory($DocumentID,$Version); 
+  &MakeDirectory($DocumentID,$Version);
+  my $Directory = &GetDirectory($DocumentID,$Version);
 
   $ShortName = &ProcessUpload($Directory,$Archive{File});
   my $Status = &ExtractArchive($Directory,$ShortName); # FIXME No status yet
@@ -137,9 +138,9 @@ sub AddArchive (%) {
       push @FileIDs,$FileID;
     } else {
       push @WarnStack,"The main file $main_file did not exist or was blank.";
-    }  
-  } 
-  return @FileIDs; 
+    }
+  }
+  return @FileIDs;
 }
 
 sub AbbreviateFileName {
@@ -147,13 +148,13 @@ sub AbbreviateFileName {
 # Try to intelligently abbreivate a filename. Keep the extension if possible.
 
   my %Params = @_;
-  
-  my $FileName  = $Params{-filename};   
-  my $MaxLength = $Params{-maxlength} || 20;   
-  my $MaxExt    = $Params{-maxext}    || 4; 
+
+  my $FileName  = $Params{-filename};
+  my $MaxLength = $Params{-maxlength} || 20;
+  my $MaxExt    = $Params{-maxext}    || 4;
 
   my $ReturnString = $FileName;
-  
+
   if (length($FileName) > $MaxLength) {
     my @Parts = split /\./,$FileName;
     my $Extension = pop @Parts;
@@ -166,38 +167,38 @@ sub AbbreviateFileName {
       my $StartString = substr $FileName,0,($MaxLength-$MaxExt-3);
       my $EndString   = substr $FileName,-$MaxExt,$MaxExt;
       $ReturnString = $StartString."...".$EndString;
-    }   
+    }
   }
-  
+
   return $ReturnString;
-}  
+}
 
 sub StreamFile (%) {
   use File::MimeInfo;
- 
+
   my %Params = @_;
-  
+
   my $File = $Params{-file};
-  
+
   my $MimeType;
-  
-  if (-e $File) {  
-    my $Size = (stat $File)[7];  
-    
+
+  if (-e $File) {
+    my $Size = (stat $File)[7];
+
     $MimeType = mimetype($File);              # Try Mime-info first
     unless ($MimeType) {
       $MimeType = `$FileMagic -ib \"$File\"`; # Use magic as a backup
       chomp $MimeType;
       print STDERR "DocDB: MIME info not found, defaulting to \"magic\" which says: $MimeType\n";
     }
-    
+
     my @Parts = split /\//,$File;
     my $ShortFile = pop @Parts;
     select STDOUT;
     $| = 1;
     print "Content-Type: $MimeType\n", # Print header
-          "Content-Disposition: filename=\"$ShortFile\"\n", 
-          "Content-Length: $Size\n\n"; 
+          "Content-Disposition: filename=\"$ShortFile\"\n",
+          "Content-Length: $Size\n\n";
 
     open OUT, "<$File" or die "Cannot open File\n";
     binmode OUT if -B $File;
@@ -219,7 +220,7 @@ sub StreamFile (%) {
     print $query -> start_html,
           "There was a problem. File $File does not exist.",
           $query -> end_html;
-  }        
+  }
 }
 
 1;
