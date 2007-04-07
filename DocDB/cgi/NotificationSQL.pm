76a3a34bf902a97dd3cf37cb7cdac690ad1a5ea3
vondo <>
2007-04-07 20:47:24 +0000

    Bulk inserter for Marcia/CD

--- DocDB/cgi/NotificationSQL.pm
+++ DocDB/cgi/NotificationSQL.pm
@@ -3,7 +3,7 @@
 #    This file is part of DocDB.
 
 #    DocDB is free software; you can redistribute it and/or modify
-#    it under the terms of version 2 of the GNU General Public License 
+#    it under the terms of version 2 of the GNU General Public License
 #    as published by the Free Software Foundation.
 
 #    DocDB is distributed in the hope that it will be useful,
@@ -42,29 +42,29 @@ sub FetchEmailUser ($) {
   my $UserFetch   = $dbh -> prepare(
     "select EmailUserID,Username,Password,Name,EmailAddress,PreferHTML,CanSign,Verified,AuthorID ".
     "from EmailUser where EmailUserID=?");
-  
+
   if ($EmailUser{$eMailUserID}{EmailUserID}) {
     return $EmailUser{$eMailUserID}{EmailUserID};
-  }  
-  
+  }
+
   $UserFetch -> execute($eMailUserID);
-  
+
   ($EmailUserID,$Username,$Password,$Name,$EmailAddress,$PreferHTML,$CanSign,$Verified,$AuthorID) = $UserFetch -> fetchrow_array;
-  
+
   if ($Verified != 1) { # Have some weird ones out there
     $Verified = 0;
-  }  
-  
+  }
+
   $EmailUser{$EmailUserID}{EmailUserID}  = $EmailUserID;
   $EmailUser{$EmailUserID}{Username}     = $Username;
   $EmailUser{$EmailUserID}{Password}     = $Password;
-  $EmailUser{$EmailUserID}{Name}         = $Name; # Construct from first/last 
+  $EmailUser{$EmailUserID}{Name}         = $Name; # Construct from first/last
   $EmailUser{$EmailUserID}{EmailAddress} = $EmailAddress;
   $EmailUser{$EmailUserID}{PreferHTML}   = $PreferHTML;
   $EmailUser{$EmailUserID}{CanSign}      = $CanSign;
   $EmailUser{$EmailUserID}{Verified}     = $Verified;
   $EmailUser{$EmailUserID}{AuthorID}     = $AuthorID;
-  
+
   return $EmailUser{$EmailUserID}{EmailUserID};
 }
 
@@ -76,12 +76,12 @@ sub ClearNotifications {
 sub DeleteNotifications ($) {
   my ($ArgRef) = @_;
   my $EmailUserID = exists $ArgRef->{-emailuserid} ? $ArgRef->{-emailuserid} : 0;
-  
+
   if ($EmailUserID) { # FIXME: Don't do document specific?
     my $Delete = $dbh -> prepare("delete from Notification where EmailUserID=?");
     $Delete -> execute($EmailUserID);
   }
-  
+
   return 0;
 }
 
@@ -92,9 +92,9 @@ sub InsertNotifications ($) {
   my $Type        = exists $ArgRef->{-type}        ?   $ArgRef->{-type}        : 0;
   my @IDs         = exists $ArgRef->{-ids}         ? @{$ArgRef->{-ids}}        : ();
   my @TextKeys    = exists $ArgRef->{-textkeys}    ? @{$ArgRef->{-textkeys}}   : ();
-  
+
   # FIXME: Need way to insert AllDocuments
-  
+
   my $Count = 0;
   if ($EmailUserID && $Period && $Type && (@IDs || @TextKeys)) {
     my $Insert = $dbh -> prepare(
@@ -108,29 +108,29 @@ sub InsertNotifications ($) {
     foreach my $ID (@IDs) {
       $Insert -> execute($EmailUserID,$Type,$ID,$Period);
       ++$Count;
-    }  
+    }
     foreach my $TextKey (@TextKeys) {
       $TextInsert -> execute($EmailUserID,$Type,$TextKey,$Period);
       ++$Count;
-    }  
+    }
   }
   return $Count;
-}  
+}
 
 sub FetchNotifications ($) {
   my ($ArgRef) = @_;
   my $EmailUserID = exists $ArgRef->{-emailuserid} ? $ArgRef->{-emailuserid} : 0;
 
   my $Count = 0;
-  
+
   if ($EmailUserID) {
     %{$Notifications{$EmailUserID}} = (); # Erase notifications for user, not all users
-    
+
     my ($Type,$ForeignID,$Period,$TextKey);
     my $Fetch = $dbh -> prepare("select Type,ForeignID,Period,TextKey from Notification where EmailUserID=?");
     $Fetch -> execute($EmailUserID);
     $Fetch -> bind_columns(undef,\($Type,$ForeignID,$Period,$TextKey));
-    
+
     while ($Fetch -> fetch) {
       my $Key = $Type."_".$Period;
       if ($TextKey) {
@@ -138,21 +138,21 @@ sub FetchNotifications ($) {
       } elsif ($ForeignID) {
         push @{$Notifications{$EmailUserID}{$Key}},$ForeignID;
       }
-      ++$Count;      
+      ++$Count;
     }
   }
-  
+
   return $Count;
 }
 
 sub InsertEmailDocumentImmediate (%) {
   my %Params = @_;
-  
+
   my $EmailUserID = $Params{-emailuserid};
   my $DocumentID  = $Params{-docid};
-  
-  InsertNotifications({ -emailuserid => $EmailUserID, -period => "Immediate", 
-                        -type        => "Document", -ids => [$DocumentID] }); 
+
+  InsertNotifications({ -emailuserid => $EmailUserID, -period => "Immediate",
+                        -type        => "Document", -ids => [$DocumentID] });
 }
 
 
