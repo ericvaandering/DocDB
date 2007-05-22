4a3e9752c8d0c9edfddefeca259bd29b06f2b645
vondo <>
2007-05-22 02:11:33 +0000

    CHANGES

--- DocDB/cgi/DocDBVersion.pm
+++ DocDB/cgi/DocDBVersion.pm
@@ -15,6 +15,6 @@
 #    along with DocDB; if not, write to the Free Software
 #    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-$DocDBVersion = "8.5.0";
+$DocDBVersion = "8.5.1";
 
 1;
