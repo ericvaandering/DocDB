3829fee97cea0c91e6e7be31db2cc509aa4d8207
vondo <>
2007-09-25 16:53:03 +0000

    Ignore blank lines in sign-offs

--- DocDB/cgi/DocDBVersion.pm
+++ DocDB/cgi/DocDBVersion.pm
@@ -20,6 +20,6 @@
 #    along with DocDB; if not, write to the Free Software
 #    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-$DocDBVersion = "8.5.8";
+$DocDBVersion = "8.5.9";
 
 1;
