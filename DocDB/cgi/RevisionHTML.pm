a2068720724ab7ae1ba48a5a7ac6b1ea90f00000
vondo <>
2010-06-16 08:16:18 +0000

    Add Clone function

--- DocDB/cgi/RevisionHTML.pm
+++ DocDB/cgi/RevisionHTML.pm
@@ -24,6 +24,7 @@
 #    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 sub TitleBox (%) {
+  push @DebugStack,"tb TitleDefault $TitleDefault";
   my (%Params) = @_;
   #FIXME: Get rid of global default
 
