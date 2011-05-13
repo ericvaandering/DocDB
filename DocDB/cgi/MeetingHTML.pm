ca34f9d04c9bf204e5978bdb3545db8430ff93ff
vondo <>
2011-05-13 21:09:16 +0000

    Fix issue where Schedule Similar mixed data between sessions

--- DocDB/cgi/MeetingHTML.pm
+++ DocDB/cgi/MeetingHTML.pm
@@ -202,12 +202,11 @@ sub SessionEntryForm (%) {
 
     print "<td rowspan=\"2\">";
     if ($OffsetDays) {  # We are copying, not modifiying the original
-      $query -> param('meetingorderid',"n$SessionOrder"); #FIXME: Try to remove
-      print $query -> hidden(-name => 'meetingorderid', -default => "n$SessionOrder");
-    } else {
-      $query -> param('meetingorderid',$MeetingOrderID); #FIXME: Try to remove
-      print $query -> hidden(-name => 'meetingorderid', -default => $MeetingOrderID);
+      $MeetingOrderID = "n$SessionOrder";
     }
+    $query -> param('meetingorderid',$MeetingOrderID); #FIXME: Try to remove
+    print $query -> hidden(-name => 'meetingorderid', -default => $MeetingOrderID);
+
     SessionOrder();                     print "<br/>\n";
     SessionModifyLink($MeetingOrderID); print "<br/>\n";
     SessionDelete($MeetingOrderID);     print "<br/>\n";
@@ -277,6 +276,7 @@ sub SessionSeparator ($) {
 
   if ($SessionSeparatorDefault eq "Yes") {
     print "Break\n";
+    print $query -> hidden(-name => "sessionseparator", -default => "$MeetingOrderID");
   } elsif ($SessionSeparatorDefault eq "No") {
     print "\n";
   } else {
