32bbb20d966d407138a9688a844d804ea891ed4f
vondo <>
2012-04-09 13:56:56 +0000

    Requester -> submitter

--- DocDB/cgi/GeneralInstructions.pm
+++ DocDB/cgi/GeneralInstructions.pm
@@ -9,7 +9,7 @@
 #
 #      Author: Eric Vaandering (ewv@fnal.gov)
 
-# Copyright 2001-2011 Eric Vaandering, Lynn Garren, Adam Bryant
+# Copyright 2001-2012 Eric Vaandering, Lynn Garren, Adam Bryant
 #    Additional Text: Marcia Teckenbrock
 
 #    This file is part of DocDB.
@@ -346,7 +346,7 @@ HTML
   When <strong>reserving</strong> a document, you must supply:</p>
   <ul>
    <li>A title </li>
-   <li>A requester (who is requesting the document?)</li>
+   <li>A submitter (who is requesting the document?)</li>
    <li>A document type (talk, note, etc.)</li>
   </ul>
 
@@ -514,7 +514,7 @@ AUTHEND
   forms to your liking and to save some typing by telling the database who you
   are. </p>
 
-  <p>Once the database knows who you are, your name will be pre-selected as the requester and
+  <p>Once the database knows who you are, your name will be pre-selected as the submitter and
   author of new documents. You can, of course, change this setting to enter
   documents by people other than (or in addition to) yourself.</p>
 
