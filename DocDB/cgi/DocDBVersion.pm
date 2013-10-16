b4e577b1e6c32447edc0124cd693038d21fa0e28
Eric Vaandering <ericvaandering@gmail.com>
2013-10-16 08:48:57 -0500

    RC5

--- DocDB/cgi/DocDBVersion.pm
+++ DocDB/cgi/DocDBVersion.pm
@@ -20,6 +20,6 @@
 #    along with DocDB; if not, write to the Free Software
 #    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-$DocDBVersion = "8.7.20rc4";
+$DocDBVersion = "8.7.20rc5";
 
 1;
