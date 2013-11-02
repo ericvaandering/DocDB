95569dc1c06ad35369b7ffd4b3f621c6ce1c3b0b
Eric Vaandering <ericvaandering@gmail.com>
2013-11-02 09:49:07 -0500

    Get correct variable for copying and Regexp allows for list of strings with embedded line breaks

--- DocDB/cgi/UntaintListOfHTML.pm
+++ DocDB/cgi/UntaintListOfHTML.pm
@@ -30,7 +30,8 @@ use strict;
 use base 'CGI::Untaint::listofwords';
 use HTML::Entities qw(encode_entities_numeric);
 
-sub _untaint_re { qr/^(.*(\000)*)+$/ }
+# Could also add s modifier to regex and .* instead of (.|\n)*
+sub _untaint_re { qr/^((.|\n)*(\000)*)+$/ }
 
 sub is_valid {
   my $self = shift;
