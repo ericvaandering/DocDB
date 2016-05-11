ericvaandering/DocDB

commit 7b18a766027afe340ccae286f24e5f75bf3cd6be

Use typed int for a couple of things

DocDB/cgi/DeleteConfirm
@@ -52,11 +52,8 @@ $dbh   = DBI->connect('DBI:mysql:'.$db_name.':'.$db_host,$db_rouser,$db_ropass);
 
 my $Untaint = CGI::Untaint -> new($query -> Vars);
 
-my $DocumentID = $Untaint -> extract(-as_integer => "documentid") || 0;
-
-my $DelVersion = $params{version};
-$DelVersion =~ s/^\s+//; # Remove leading and trailing spaces
-$DelVersion =~ s/\s+$//;
+my $DocumentID = $Untaint -> extract(-as_typedint => "documentid") || 0;
+my $DocumentID = $Untaint -> extract(-as_typedint => "version") || 0;
 
 @ErrorStack = ();
 

DocDB/cgi/DeleteDocument
@@ -41,8 +41,8 @@ $query = new CGI;  # Global for subroutines
 $query -> autoEscape(0);
 my $Untaint = CGI::Untaint -> new($query -> Vars);
 
-my $DocumentID = $Untaint -> extract(-as_integer => "docid") || 0;
-my $Version = $Untaint -> extract(-as_integer => "version") || 0;
+my $DocumentID = $Untaint -> extract(-as_typedint => "docid") || 0;
+my $Version = $Untaint -> extract(-as_typedint => "version") || 0;
 my $Password = $Untaint -> extract(-as_printable => "password") || "";
 my $Username = $Untaint -> extract(-as_printable => "admuser") || "";
 

commit d7e7327e9b122ed602ba1b170151d0a849664db7

Restore the ability to delete a document, broken in 7b18a766027afe340ccae286f24e5f75bf3cd6be

DocDB/cgi/DeleteConfirm
@@ -53,7 +53,7 @@ $dbh   = DBI->connect('DBI:mysql:'.$db_name.':'.$db_host,$db_rouser,$db_ropass);
 my $Untaint = CGI::Untaint -> new($query -> Vars);
 
 my $DocumentID = $Untaint -> extract(-as_typedint => "docid") || 0;
-my $DocumentID = $Untaint -> extract(-as_typedint => "version") || 0;
+my $DelVersion = $Untaint -> extract(-as_typedint => "version") || 0;
 
 @ErrorStack = ();
 

DocDB/cgi/DocDBVersion.pm
@@ -20,6 +20,6 @@
 #    along with DocDB; if not, write to the Free Software
 #    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-$DocDBVersion = "8.8.7p2";
+$DocDBVersion = "8.8.7p3";
 
 1;