544c6881239774060a135e57472c9c880f3bd7f8
Eric Vaandering <ericvaandering@gmail.com>
2018-05-21 14:47:43 -0500

    Try to get ordered by text working

--- DocDB/html/js/AuthorMethodDisplay.js
+++ DocDB/html/js/AuthorMethodDisplay.js
@@ -10,3 +10,16 @@ function author_method_choose(method) {
     document.getElementById("AuthorText").style.display   = "none";
   }
 }
+
+function author_method_choose_text(method) {
+  if (method == "text") {
+    document.getElementById("AuthorActiveSearch").style.display = "none";
+    document.documentadd.authormode.value = "field";
+    document.getElementById("AuthorText").style.display   = "";
+  }
+  if (method == "active") {
+    document.getElementById("AuthorActiveSearch").style.display = "";
+    document.documentadd.authormode.value = "list";
+    document.getElementById("AuthorText").style.display   = "none";
+  }
+}
