9e259c3f568bb91a7eda957153bfc2afeb41c61b
Eric Vaandering <ericvaandering@gmail.com>
2018-06-19 15:45:51 -0500

    Bump version #

--- DocDB/cgi/DocDBVersion.pm
+++ DocDB/cgi/DocDBVersion.pm
@@ -21,6 +21,6 @@
 #    along with DocDB; if not, write to the Free Software
 #    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-$DocDBVersion = "8.8.9sso";
+$DocDBVersion = "8.8.9";
 
 1;
