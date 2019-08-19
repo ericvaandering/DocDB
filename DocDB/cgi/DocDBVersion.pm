56bdd2f7cc16b83850b226881a8635716994b879
Eric Vaandering <ericvaandering@gmail.com>
2019-08-19 11:36:03 -0500

    Get rid of another XSS issue

--- DocDB/cgi/DocDBVersion.pm
+++ DocDB/cgi/DocDBVersion.pm
@@ -21,6 +21,6 @@
 #    along with DocDB; if not, write to the Free Software
 #    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-$DocDBVersion = "8.8.9p3";
+$DocDBVersion = "8.8.9p4";
 
 1;
